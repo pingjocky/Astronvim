1be32e0ec171265fc15679ca0eba0281d170af12
Aaron Taddei <aarontaddei@gmail.com>
2023-04-03 23:57:13 -0400

    Changed user settings

---
+++ plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    -- TODO: Add more themes!!
+    "rmehri01/onenord.nvim",
+    "luisiacc/gruvbox-baby",
+    "patstockwell/vim-monokai-tasty",
+    "savq/melange-nvim",
+    "folke/tokyonight.nvim",
+    "tanvirtin/monokai.nvim",
+    opts = {},
+  },
+}
--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,8 +5,9 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = { "lua_ls", "jedi_language_server", },
     },
+    event = "User AstroFile",
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
